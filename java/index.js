8772bfd48694d017633bbf242f9a442c653267f4
Martinaroque <124635423+Martinaroque@users.noreply.github.com>
2023-07-25 17:41:36 +0100

    update js

--- java/index.js
+++ java/index.js
@@ -1,27 +1,46 @@
+function updateTimeLisbon(){
+
 let lisbonElement= document.querySelector("#lisbon");
 let lisbonDate = lisbonElement.querySelector(".date")
 let lisbonTime = lisbonElement.querySelector(".time")
 let lisbonTimeElement = moment(); 
 lisbonDate.innerHTML= lisbonTimeElement.format("dddd, Do MMMM YYYY");
 lisbonTime.innerHTML = `${lisbonTimeElement.format("h:mm:ss [<small>]A[</small>]")} `
+}
+updateTimeLisbon();
+setInterval(updateTimeLisbon,1000)
+
 ////
+function updateTimeParis(){
 let parisElement= document.querySelector("#paris");
 let parisDate = parisElement.querySelector(".date")
 let parisTime = parisElement.querySelector(".time")
 let parisTimeElement = moment().tz("Europe/Paris"); 
 parisDate.innerHTML= parisTimeElement.format("dddd, Do MMMM YYYY");
 parisTime.innerHTML = `${parisTimeElement.format("h:mm:ss [<small>]A[</small>]")} `
+}
+updateTimeParis();
+setInterval(updateTimeParis,1000)
 ////
+
+function updateTimeNewYork(){
 let newYorkElement= document.querySelector("#new-york");
 let newYorkDate = newYorkElement.querySelector(".date")
 let newYorkTime = newYorkElement.querySelector(".time")
 let newYorkTimeElement = moment().tz("America/New_York"); 
 newYorkDate.innerHTML= newYorkTimeElement.format("dddd, Do MMMM YYYY");
 newYorkTime.innerHTML = `${newYorkTimeElement.format("h:mm:ss [<small>]A[</small>]")} `
+} 
+updateTimeNewYork();
+setInterval(updateTimeNewYork,1000)
 ////
+function updateTimeSydney(){
 let sydneyElement= document.querySelector("#sydney");
 let sydneyDate = sydneyElement.querySelector(".date")
 let sydneyTime = sydneyElement.querySelector(".time")
 let sydneyTimeElement = moment().tz("Australia/Sydney"); 
 sydneyDate.innerHTML= sydneyTimeElement.format("dddd, Do MMMM YYYY");
 sydneyTime.innerHTML = `${sydneyTimeElement.format("h:mm:ss [<small>]A[</small>]")} `
+}
+updateTimeSydney();
+setInterval(updateTimeSydney,1000)
